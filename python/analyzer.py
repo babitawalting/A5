3a0f324dd28826038a6ed11eacaceb6e75c0b9b3
bwalting <babita.alting@hotmail.com>
2024-12-09 14:22:15 +0100

    Update analyzer.py

--- python/analyzer.py
+++ python/analyzer.py
@@ -20,7 +20,13 @@ dataBavaria = dataBavaria.sort_values("datum")
 
 
 #Informatievraag 3
+dataSorted = dataBavaria.sort_values("overtredingen", ascending=False)
+top10 = dataSorted.head(10)
+print(top10)
 
+file3 = open("files/zwartboek.txt", "w", encoding="UTF-8")
+file3.write(bamboo.prettify(dataSorted, type="zwartboek"))
+ #Deze regel nog invullen! Hoe sluit je file3?
 
 
 #Informatievraag 4
