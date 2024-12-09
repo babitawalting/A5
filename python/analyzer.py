66cc4958ef038295b2528bcc4f8518b35932f985
Freek Voermans <104316344+CandleDev1@users.noreply.github.com>
2024-12-09 13:55:35 +0100

    Update analyzer.py

--- python/analyzer.py
+++ python/analyzer.py
@@ -10,17 +10,6 @@ dataBavaria = pd.read_excel("A5/excelfiles/Voetbal_Bavaria League_tussenstand.xl
 dataBavaria["datum"] = pd.to_datetime(dataBavaria["datum"], format="%d/%m/%Y")
 dataBavaria = dataBavaria.sort_values("datum")
 
-dataBrand = pd.read_excel("A5/excelfiles/Voetbal_Brand League_tussenstand.xlsx")
-dataBrand["datum"] = pd.to_datetime(dataBrand["datum"], format="%d/%m/%Y")
-dataBrand = dataBrand.sort_values("datum")
-
-dataHeineken = pd.read_excel("A5/excelfiles/Voetbal_Heineken League_tussenstand.xlsx")
-dataHeineken["datum"] = pd.to_datetime(dataHeineken["datum"], format="%d/%m/%Y")
-dataHeineken = dataHeineken.sort_values("datum")
-
-dataJupiler = pd.read_excel("A5/excelfiles/Voetbal_Jupiler League_tussenstand.xlsx")
-dataJupiler["datum"] = pd.to_datetime(dataJupiler["datum"], format="%d/%m/%Y")
-dataJupiler = dataJupiler.sort_values("datum")
 
 #Informatievraag 1
 
