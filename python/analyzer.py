368b8a573a4d8b588f0a5d0a8a6779a2efd20f6f
bwalting <babita.alting@hotmail.com>
2024-12-10 09:38:41 +0100

    Update analyzer.py

--- python/analyzer.py
+++ python/analyzer.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import datetime
+from datetime import timedelta
 import lib_bamboo as bamboo
 import os
 
@@ -26,6 +27,7 @@ file2.write(f"{averageViolations}")
 file2.close()
 
 #Informatievraag 3
+
 dataSorted = dataBavaria.sort_values("overtredingen", ascending=False)
 top10 = dataSorted.head(10)
 
@@ -34,8 +36,19 @@ file3.write(bamboo.prettify(top10, type="zwartboek"))
 file3.close()
 
 
-#Informatievraag 4
+#Informatievraag 4 
+
+today = datetime.now()
+twoWeeksAgo = today - timedelta(days=14)
+filter1 = (dataBavaria["overtredingen"] <= 2)
+hallOfFame = dataBavaria[filter1]
+filter2 = (dataBavaria["datum"] < today) and (hallOfFame["datum"] > twoWeeksAgo)
+hallOfFame = hallOfFame[filter2]
 
 
+file4 = open("files/eregalerij.txt", "a", encoding="UTF-8")
+file4.write(bamboo.prettify(hallOfFame, type="eregalerij"))
+file4.close()
 
 print("Done!")
+
