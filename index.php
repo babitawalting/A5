0f3626fb17259a567d696976de2b23349a077cc5
bwalting <babita.alting@hotmail.com>
2024-12-10 10:28:37 +0100

    aaaaaaaaaaaaaa

--- index.php
+++ index.php
@@ -12,7 +12,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
 
     <!-- Hier worden de fonts al voor je ingeladen! -->
-    <link href="https://fonts.googleapis.com/css2?family=Faster+One&family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500&family=Roboto+Mono:wght@400;500&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Faster+One&family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500&family=Roboto+Mono:wght@400;500&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
@@ -24,20 +26,39 @@
         </header>
 
         <main>
-            <p>De SPORT-bond ziet toe op een eerlijk verloop van de competitie. Onze topsporters dienen een voorbeeld te zijn voor de vele amateurs in de sport. Daarom streven we naar <em>fair play</em>; een sportieve omgang met elkaar. Daar hoort ook bij dat er weinig overtredingen plaatsvinden tijdens een wedstrijd. Op deze website geven we inzicht in het verloop van de competitie tot nu toe. U ziet de wedstrijden met de minste overtredingen, maar voor bewustwording brengen we ook de wedstrijden met de <em>meeste</em> overtredingen in beeld.</p>
+            <p>De SPORT-bond ziet toe op een eerlijk verloop van de competitie. Onze topsporters dienen een voorbeeld te
+                zijn voor de vele amateurs in de sport. Daarom streven we naar <em>fair play</em>; een sportieve omgang
+                met elkaar. Daar hoort ook bij dat er weinig overtredingen plaatsvinden tijdens een wedstrijd. Op deze
+                website geven we inzicht in het verloop van de competitie tot nu toe. U ziet de wedstrijden met de
+                minste overtredingen, maar voor bewustwording brengen we ook de wedstrijden met de <em>meeste</em>
+                overtredingen in beeld.</p>
             <div class="row">
                 <div class="box half">
                     <h2>Aantal overtredingen:</h2>
                     <p class="number"><?php require_once("./files/total.txt") ?></p>
 
 
-                    <pre><?php require_once("./files/zwartboek.txt") ?></pre>
+                    
+                </div>
+
+                <div class="row">
+                    <div class="box">
+                        <h2>Wedstrijden met de meeste overtredingen:</h2>
+                        <pre><?php require_once("./files/zwartboek.txt") ?></pre>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="box">
+                        <h2>Wedstrijden met minder dan twee overtredingen(laatste 14 dagen)</h2>
+                        <pre><?php require_once("./files/eregalerij.txt") ?></pre>
+                    </div>
                 </div>
             </div>
         </main>
 
         <footer>
-            <p>Deze website is gemaakt in het kader van een praktijkopdracht bij de opleiding Software Developer, Curio Breda.</p>
+            <p>Deze website is gemaakt in het kader van een praktijkopdracht bij de opleiding Software Developer, Curio
+                Breda.</p>
             <p>&copy; Team X, Pietje Puk en Jan Smit.</p>
         </footer>
 
